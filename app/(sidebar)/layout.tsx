30e8ad3939349064d964a361f6f508c33b266ec1
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-09-21 16:06:25 +0700

    :lipstick: (ui) change sidebar width

--- app/(sidebar)/layout.tsx
+++ app/(sidebar)/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="flex gap-2 h-screen w-screen p-2">
-      <div className="bg-[#f7f7f7] border border-[#f1f1f1] h-full w-[280px] shadow-sm rounded-xl">
+      <div className="bg-[#f7f7f7] border border-[#f1f1f1] h-full w-[270px] shadow-sm rounded-xl">
         <h1 className="font-bold text-2xl text-center mt-8 tracking-tight">
           Kopixel
         </h1>
